2a2908c1a654a6c1f5733c7e9aba62fd117ea3e4
dbeley <6568955+dbeley@users.noreply.github.com>
2019-05-19 12:01:17 +0200

    generic preparation script

--- preparation/extract_column_from_df.py
+++ preparation/extract_column_from_df.py
@@ -14,18 +14,28 @@ def main():
         logger.error("No file entered. Use -f flag.")
         exit()
 
-    df = pd.read_excel(args.file)
+    ext = Path(args.file).suffix
+    logger.debug("extension detected : %s", ext)
+    if ext == '.csv':
+        df = pd.read_csv(args.file, sep='\t')
+    elif ext == '.xlsx':
+        df = pd.read_excel(args.file)
+    else:
+        logger.error("Extension not supported. Exiting")
+        exit()
 
     Path("Datasets").mkdir(parents=True, exist_ok=True)
-    df['Comment'].to_csv(f"Datasets/Export_{Path(args.file).stem}.csv", index=False)
+
+    df[args.column_name].to_csv(f"Datasets/Export_{Path(args.file).stem}.csv", index=False)
 
     logger.info("Runtime : %.2f seconds" % (time.time() - temps_debut))
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Script textgenrnn_init')
+    parser = argparse.ArgumentParser(description='Extract a column from a csv or xlsx file')
     parser.add_argument('--debug', help="Display debugging information", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
-    parser.add_argument('-f', '--file', help="Text file to train the model from", type=str)
+    parser.add_argument('-f', '--file', help="Text file to extract the column from", type=str)
+    parser.add_argument('-c', '--column_name', help="Name of the column to extract (default = Comments)", type=str, default="Comments")
     args = parser.parse_args()
 
     logging.basicConfig(level=args.loglevel)
